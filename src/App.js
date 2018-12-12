5700ff9cf06dbcdaa75263bd099efefef9209ae9
Derrick <derrickhuang2333@gmail.com>
2018-12-12 18:40:45 +1000

    test

--- src/App.js
+++ src/App.js
@@ -173,7 +173,7 @@ class App extends Component {
   }
 
   componentDidMount(){
-    fetch('http://localhost:3001/'),then(response=>response.json())
+    fetch('http://localhost:3001/').then(response=>response.json())
     .then(data=>console.log(data));
   }
 
